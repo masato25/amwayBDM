55c807cd836959c0e597510f00c3474ff4f06314
masato <help0999@gmail.com>
2016-12-08 21:48:44 +0800

    add mock data for testing

--- app/controllers/machines_controller.rb
+++ app/controllers/machines_controller.rb
@@ -86,8 +86,11 @@ class MachinesController < ApplicationController
                     end
                     xml.HealthyFeature d["HealthyFeature"]
                     xml.FeatureImagePath d.feature_images
-                    xml.CompositionImagePath d.composition_images
+                    xml.OtherImagePath d.composition_images
                     xml.HealthyTip d["HealthyTip"]
+                    xml.QRCodeImagePath "/system/detail_images/images/000/000/035/original/ak-1Q.jpg?1481204849"
+                    xml.ContentImagePath "/system/detail_images/images/000/000/034/original/S00.png?1481204365"
+                    xml.SeriesBackgroundImagePath "/system/detail_images/images/000/000/033/original/bg_nutrilife.png?1481204237"
                     xml.Class do
                       d.p_metadata.each do |pmt|
                         xml.Metadata do
@@ -95,7 +98,7 @@ class MachinesController < ApplicationController
                           xml.Volume pmt["Volume"]
                           xml.Price pmt["Price"]
                           xml.Color pmt["Color"]
-                          xml.QrImagePath pmt.qr_image
+                          # xml.QrImagePath pmt.qr_image
                         end
                       end
                     end
